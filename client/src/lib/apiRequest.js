448349b67d4de9a7cf391d61fe4f05541da98515
Umang Raval <umangraval0365@gmail.com>
2024-11-06 21:17:06 +0530

    uploading

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
-  // baseURL: "https://realestate-website-1.onrender.com/api/",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8800/api/",
   withCredentials: true,
 });
 
