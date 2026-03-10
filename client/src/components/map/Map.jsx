421b21bbd6c0dcbfcd91c2194dce0f628398d22d
Umang Raval <umangraval0365@gmail.com>
2026-03-11 00:20:36 +0530

    solved mark not apperaing in map

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -2,6 +2,18 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import "./map.scss";
 import "leaflet/dist/leaflet.css";
 import Pin from "../pin/Pin";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 
 function Map({ items }) {
   return (
