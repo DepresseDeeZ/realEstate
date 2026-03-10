539ba7b63018a1db48348a7f096a6a7b3d0df7a5
Umang Raval <umangraval0365@gmail.com>
2026-03-10 21:59:07 +0530

    new changes

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  // baseURL: "http://localhost:8800/api",
+  baseURL: "https://realestate-backend-api-9y65.onrender.com/api",
   withCredentials: true,
 });
 
